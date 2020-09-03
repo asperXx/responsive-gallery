4cb651873202d3752a59078f87116732f06f118f
AsperXx <varfol0077@gmail.com>
2020-09-03 21:45:43 +0500

    add d&d func

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,42 +1,103 @@
 <template>
   <div class="container">
-    <div class="gallery">
-      dfgdfg
-      <!-- <a
-        v-for="(img,id) in pictures"
+    <div class="gallery" ref="fileform">
+      <a
+        v-for="(img,id) in IMAGES"
         :key="id"
         class="gallery__item"
         :style="{ width: (justifyScale * img.width / img.height) + 'px', flexGrow: img.width / img.height}"
       >
         <img :src="img.url" />
-      </a> -->
+      </a>
     </div>
   </div>
 </template>
 
 <script>
+import { mapGetters, mapActions } from "vuex";
+
 export default {
-  data() {
-    return {
-      justifyScale: screen.height * 0.2,
-    };
+   data(){
+      return {
+        dragAndDropCapable: false,
+        files: [],
+        uploadPercentage: 0,
+        src: "",
+        justifyScale: screen.height * 0.2,
+        imgHeight: null,
+        imgWidth: null
+      }
+    },
+    mounted(){
+      this.dragAndDropCapable = this.determineDragAndDropCapable();
+      if( this.dragAndDropCapable ){
+        ['drag', 'dragstart', 'dragend', 'dragover', 'dragenter', 'dragleave', 'drop'].forEach( function( evt ) {
+          this.$refs.fileform.addEventListener(evt, function(e){
+            e.preventDefault();
+            e.stopPropagation();
+          }.bind(this), false);
+        }.bind(this));
+        this.$refs.fileform.addEventListener('drop', function(e){
+          for( let i = 0; i < e.dataTransfer.files.length; i++ ){
+            this.files.push( e.dataTransfer.files[i] );
+            this.setImage();
+          }
+        }.bind(this));
+      }
+    },
+    methods: {
+      ...mapActions(["GET_IMAGE_FROM_URL"]),
+      determineDragAndDropCapable(){
+        var div = document.createElement('div');
+        return ( ( 'draggable' in div )
+                || ( 'ondragstart' in div && 'ondrop' in div ) )
+                && 'FormData' in window
+                && 'FileReader' in window;
+      },
+      setImage(){
+        for( let i = 0; i < this.files.length; i++ ){
+          if ( /\.(jpe?g|png|gif)$/i.test( this.files[i].name ) ) {
+            this.findSizeImg();
+            let reader = new FileReader();
+            reader.addEventListener("load", function(){
+              this.src = reader.result;
+              const picture = {
+              url: this.src,
+              width: this.imgWidth,
+              height: this.imgHeight,
+            };
+            console.log(picture);
+            this.GET_IMAGE_FROM_URL(picture);
+            }.bind(this), false);
+            reader.readAsDataURL( this.files[i] );
+          } 
+        }
+      },
+       findSizeImg() {
+      var newImg = new Image();
+      newImg.onload = () => {
+        try {
+          this.imgHeight = newImg.height;
+          this.imgWidth = newImg.width;
+        } catch (e) {
+          console.log(e);
+        }
+      };
+      newImg.src = this.url;
+    },
+  
+},
+computed: {
+    ...mapGetters(["IMAGES"])
   },
-  computed: {},
-};
+}
 </script>
 
-<style scoped lang="scss">
-html,
-body {
-  
-}
+<style lang="scss">
 .container {
   display: flex;
   flex-direction: column;
   align-items: center;
-  height: 800px;
-  width: 100%;
-  background: #333;
 }
 .gallery {
   display: flex;
--- src/components/UploadImages.vue
+++ src/components/UploadImages.vue
@@ -1,36 +1,77 @@
 <template>
   <div class="container">
     <div class="upload">
-      <input type="text" class="upload__url" />
+      <input type="text" class="upload__url" v-model="URL" />
       <div class="upload__buttons">
-        <div class="button">Clear</div>
+        <div class="button" @click="URL = ''">Clear</div>
         <div class="button">Switch</div>
-        <div class="button">Upload</div>  
+        <div class="button" @click="findSizeImg">Upload</div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import { mapActions } from "vuex";
+export default {
+  data() {
+    return {
+      isActive: true,
+      URL: "https://api.jsonbin.io/b/5f4e3eeb993a2e110d3c2046/1",
+      imgHeight: null,
+      imgWidth: null,
+      isDisable: true,
+    };
+  },
+  methods: {
+    ...mapActions(["GET_IMAGE_FROM_URL", "GET_IMAGES_FROM_JSON_URL"]),
+    // Добавление картинки или json файла
+    uploadImages() {
+      // const picture = {
+      // url: this.url,
+      // width: this.imgWidth,
+      // height: this.imgHeight,
+      // };
+      // console.log(picture);
+      // this.addPicture(picture);
+      // this.url = "";
+      this.GET_IMAGES_FROM_JSON_URL(this.URL);
+      // }
+    },
+    // Определение размера картинки до загрузки
+    findSizeImg() {
+      var newImg = new Image();
+      newImg.onload = () => {
+        try {
+          this.imgHeight = newImg.height;
+          this.imgWidth = newImg.width;
+          console.log(this.imgHeight, this.imgWidth)
+        } catch (e) {
+          console.log(e);
+        }
+      };
+      newImg.src = this.URL;
+    },
+  },
+};
 </script>
 
 <style lang="scss">
 .container {
-    max-width: 860px;
-    min-width: 320px;
+  max-width: 860px;
+  min-width: 320px;
 }
 .upload {
+  display: flex;
+  flex-direction: column;
+  &__buttons {
     display: flex;
-    flex-direction: column;
-    &__buttons {
-        display: flex;
-        width: 100%;
-        justify-content: space-between;
-    }
-    &__url {
-        width: 100%;
-    }
+    width: 100%;
+    justify-content: space-between;
+  }
+  &__url {
+    width: 100%;
+  }
 }
 .button {
   background-color: black;
